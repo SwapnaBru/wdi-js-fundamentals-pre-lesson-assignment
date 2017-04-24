899ed2341d20456ca4d686e5f529b91be8f30b25
Swapna Brungi <swapna.brungi@gmail.com>
2017-04-23 22:09:23 -0400

    Comparing ruby with js and examining how objects behave

---
+++ pacman-console-log.js
@@ -0,0 +1,12 @@
+console.log('Pac-Man eats ghosts for lunch');
+// I am a comment!
+
+var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+console.log('There are ' + ghosts.length + ' ghosts.');
+console.log(ghosts[0]);
+console.log(ghosts[1]);
+console.log(ghosts[2]);
+console.log(ghosts[3]);
+
+// figure out a way to print the sentence with four instead of 4
